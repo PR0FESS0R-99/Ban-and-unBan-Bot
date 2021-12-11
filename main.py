11fc077ae34ac995a3106098f5bed171f7db6565
Sourcery AI <>
2021-12-11 04:42:54 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -17,62 +17,60 @@ async def start(bot, message):
 @Pr0fess0r_99.on_message(filters.command(["ban"]))
 async def ban(bot, message):
     chatid = message.chat.id
-    if message.reply_to_message:
-        admins_list = await bot.get_chat_members(
-            chat_id=chatid, filter="administrators"
-        )
-        admins = []
-        for admin in admins_list:
-            id = admin.user.id
-            admins.append(id)
-        userid = message.from_user.id
-        if userid in admins:
-            user_to_ban = message.reply_to_message.from_user.id
-            if user_to_ban in admins:
-                await message.reply(text="Think he is Admin, Can't Ban Admins")
-            else:
-                try:
-                    await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban)
-                    await message.reply_text(
-                        f"Bye {message.reply_to_message.from_user.mention}"
-                    )
-                except Exception as error:
-                    await message.reply_text(f"{error}")
+    if not message.reply_to_message:
+        return
+    admins_list = await bot.get_chat_members(
+        chat_id=chatid, filter="administrators"
+    )
+    admins = []
+    for admin in admins_list:
+        id = admin.user.id
+        admins.append(id)
+    userid = message.from_user.id
+    if userid in admins:
+        user_to_ban = message.reply_to_message.from_user.id
+        if user_to_ban in admins:
+            await message.reply(text="Think he is Admin, Can't Ban Admins")
         else:
-            await message.reply_text("Nice try, But wrong move..")
-            return
+            try:
+                await bot.kick_chat_member(chat_id=chatid, user_id=user_to_ban)
+                await message.reply_text(
+                    f"Bye {message.reply_to_message.from_user.mention}"
+                )
+            except Exception as error:
+                await message.reply_text(f"{error}")
     else:
+        await message.reply_text("Nice try, But wrong move..")
         return
 
 @Pr0fess0r_99.on_message(filters.command(["unban"]))
 async def unban(bot, message):
     chatid = message.chat.id
-    if message.reply_to_message:
-        admins_list = await bot.get_chat_members(
-            chat_id=chatid,
-            filter="administrators"
-        )
-        admins = []
-        for admin in admins_list:
-            id = admin.user.id
-            admins.append(id)
-        userid = message.from_user.id
-        if userid in admins:
-            user_to_unban = message.reply_to_message.from_user.id
-            if user_to_uban in admins:
-                await message.reply(text="Think he is Admin, Can't Ban Admins")
-            else:
-                try:
-                    await bot.unban_chat_member(chat_id=chatid, user_id=user_to_unban)
-                    await message.reply_text(
-                        f"Welcome {message.reply_to_message.from_user.mention}"
-                    )
-                except Exception as error:
-                    await message.reply_text(f"{error}")
+    if not message.reply_to_message:
+        return
+    admins_list = await bot.get_chat_members(
+        chat_id=chatid,
+        filter="administrators"
+    )
+    admins = []
+    for admin in admins_list:
+        id = admin.user.id
+        admins.append(id)
+    userid = message.from_user.id
+    if userid in admins:
+        user_to_unban = message.reply_to_message.from_user.id
+        if user_to_uban in admins:
+            await message.reply(text="Think he is Admin, Can't Ban Admins")
         else:
-            await message.reply_text("Nice try, But wrong move..")
-            return
+            try:
+                await bot.unban_chat_member(chat_id=chatid, user_id=user_to_unban)
+                await message.reply_text(
+                    f"Welcome {message.reply_to_message.from_user.mention}"
+                )
+            except Exception as error:
+                await message.reply_text(f"{error}")
     else:
+        await message.reply_text("Nice try, But wrong move..")
         return
 
 Pr0fess0r_99.run()
